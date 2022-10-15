7869c56ecd892f61df5f6c45c028fcbf6876be8a
Mankret <asilvergrave@gmail.com>
2022-10-15 20:23:05 +0300

    Added finished homework_5.py

--- Homework/homework_5.py
+++ Homework/homework_5.py
@@ -2,8 +2,18 @@
 # Получить сумму кубов всех целых чисел от 1 до n(включая n). Исключения составляют все числа кратные цифре 3.
 # Реализовать в 2-х вариантах: используя цикл while и цикл for
 
-a = "Hello"
-b = ", world!"
+number = int(input("Введите число: "))
+csum = 0
 
-print(a + b)
+for i in range(number+1):
+    if i % 3 == 0:
+        continue
+    csum += i**3
+print(csum)
+
+while number < 0:
+    csum = range(number+1)
+    if number % 3 == 0:
+        csum += number**3
+print(csum)
 
