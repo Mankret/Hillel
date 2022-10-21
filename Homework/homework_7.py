991b135419836ca90b2fe24c0c8a1ac41eba0ff7
Mankret <asilvergrave@gmail.com>
2022-10-21 19:50:41 +0300

    Correction homework_7.py

--- Homework/homework_7.py
+++ Homework/homework_7.py
@@ -1,8 +1,9 @@
 # Дан словарь, создать новый словарь при помощи конструкции генератор словаря, поменяв местами ключ и значение.
 
 
-some_dict = {'Avalon': 1, 'Babylon': 2, 'Byzantium': 3, 'Empire': 4}
+some_dict = {1: 'Avalon', 2: 'Babylon', 3: 'Byzantium', 4: 'Empire'}
 generation_dict = {v: k for k, v in some_dict.items()}
 
 print(some_dict, generation_dict, sep='\n')
+
 
