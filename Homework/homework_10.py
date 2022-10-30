99ab2bd8313b8afb11c917e3907ec4c624875e90
Mankret <asilvergrave@gmail.com>
2022-10-30 16:15:59 +0200

    Corrected homework_10.py

--- Homework/homework_10.py
+++ Homework/homework_10.py
@@ -6,12 +6,10 @@
 
 values = [1, 2, '3', 'forth', 'end', 99, True, None]
 
-new_list = (list(map(lambda x: str(x), (values[0], values[1], values[5]))))
-values[0] = new_list[0]
-values[1] = new_list[1]
-values[5] = new_list[2]
-new_list = values
-
+new_list = list(map(lambda x: (str(x) if type(x) is int else x), values))
 
 print(new_list)
+
+
+
 
