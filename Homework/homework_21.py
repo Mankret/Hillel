4bfcec3ebf3308015568453ebb107dea02fb32f3
Mankret <asilvergrave@gmail.com>
2022-11-09 14:23:06 +0200

    Added homework_21.py

---
+++ Homework/homework_21.py
@@ -0,0 +1,35 @@
+# Создать родительский класс auto, у которого есть атрибуты:
+# brand, age, cоlor, mark и weight.
+# А так же методы: move, birthday и stop.
+# Методы move и stop выводят сообщение на экран «move» и «stop»,а birthday увеличивает атрибут age на 1.
+# Атрибуты brand, age и mark являются обязательными при объявлении объекта.
+
+class Auto:
+    brand = None
+    age = None
+    color = 'Black'
+    mark = None
+    weight = '1985 kg'
+
+    def __init__(self, brand, age, mark):
+        self.brand = brand
+        self.age = age
+        self.mark = mark
+
+    def move(self):
+        print("Move")
+
+    def birthday(self):
+        for item in range(1998, self.age + 1):
+            print(f"age:  {item}")
+
+    def stop(self):
+        print("Stop")
+
+
+car = Auto("merce", 2018, '221b')
+car.birthday()
+
+car.stop()
+car.move()
+print(car.color, car.weight, sep='\n')
